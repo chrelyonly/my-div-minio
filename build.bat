676189b582210aef0faaffa37e9a4bed7f97515f
chrelyonly <1172576293@qq.com>
2024-11-08 16:49:39 +0800

    1

--- build.bat
+++ build.bat
@@ -3,10 +3,10 @@ set GOPATH=D:\dev\dev\sdk\gopath
 set CGO_ENABLED=0
 set GOARCH=amd64
 set GOOS=windows
-D:\dev\dev\sdk\go\go1.22.5\go1.22.5\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_2_windows.exe
+D:\dev\dev\sdk\go\go1.22.5\go1.22.5\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_4_windows.exe
 set GOARCH=amd64
 set GOOS=linux
-D:\dev\dev\sdk\go\go1.22.5\go1.22.5\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_2_linux
+D:\dev\dev\sdk\go\go1.22.5\go1.22.5\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_4_linux
 set GOARCH=amd64
 set GOOS=darwin
-D:\dev\dev\sdk\go\go1.22.5\go1.22.5\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_2_darwin
+D:\dev\dev\sdk\go\go1.22.5\go1.22.5\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_4_darwin
