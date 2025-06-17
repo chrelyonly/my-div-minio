7e9bce298faeaebde3198efdca63ecc32fce7a3b
chrelyonly <1172576293@qq.com>
2025-06-17 16:33:56 +0800

    1

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
-set GOROOT=D:\dev\dev\sdk\go\go1.22.5\go1.22.5
+set GOROOT=D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.2.windows-amd64
 set GOPATH=D:\dev\dev\sdk\gopath
 set CGO_ENABLED=0
 set GOARCH=amd64
 set GOOS=windows
-D:\dev\dev\sdk\go\go1.22.5\go1.22.5\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_4_windows.exe
+D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.2.windows-amd64\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_5_windows.exe
 set GOARCH=amd64
 set GOOS=linux
-D:\dev\dev\sdk\go\go1.22.5\go1.22.5\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_4_linux
+D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.2.windows-amd64\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_5_linux
 set GOARCH=amd64
 set GOOS=darwin
-D:\dev\dev\sdk\go\go1.22.5\go1.22.5\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_4_darwin
+D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.2.windows-amd64\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_5_darwin
