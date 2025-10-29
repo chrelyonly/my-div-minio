5c65a8232e07df397794bf9415a988a57bbe1ce8
chrelyonly <1172576293@qq.com>
2025-10-29 11:23:47 +0800

    1

--- cmd/main.go
+++ cmd/main.go
@@ -19,6 +19,14 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"runtime/debug"
+	"sort"
+	"strings"
+
 	"github.com/minio/cli"
 	"github.com/minio/minio/cmd/chrelyonly"
 	"github.com/minio/minio/internal/color"
@@ -27,13 +35,6 @@ import (
 	"github.com/minio/pkg/v3/env"
 	"github.com/minio/pkg/v3/trie"
 	"github.com/minio/pkg/v3/words"
-	"io"
-	"os"
-	"path/filepath"
-	"runtime"
-	"runtime/debug"
-	"sort"
-	"strings"
 )
 
 // GlobalFlags - global flags for minio.
@@ -178,8 +179,8 @@ func startupBanner(banner io.Writer) {
 	fmt.Fprintln(banner, color.BlueBold("开源协议:")+color.Bold(" GNU AGPLv3 <https://www.gnu.org/licenses/agpl-3.0.html>"))
 	fmt.Fprintln(banner, color.BlueBold("官方版本:")+color.Bold(" %s (%s %s/%s)", ReleaseTag, runtime.Version(), runtime.GOOS, runtime.GOARCH))
 	fmt.Fprintln(banner, color.GreenBold("汉化作者: chrelyonly"))
-	fmt.Fprintln(banner, color.GreenBold("编译日期: 2024年11月8日"))
-	fmt.Fprintln(banner, color.GreenBold("div版本: 1.4"))
+	fmt.Fprintln(banner, color.GreenBold("编译日期: 2025年10月29日"))
+	fmt.Fprintln(banner, color.GreenBold("div版本: 1.6"))
 }
 
 func versionBanner(c *cli.Context) io.Reader {
