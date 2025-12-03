b529754268591994afa0cee8cffab10101992a6a
chrelyonly <1172576293@qq.com>
2025-12-03 18:00:05 +0800

    1

--- build.bat
+++ build.bat
@@ -3,10 +3,10 @@ set GOPATH=D:\dev\dev\sdk\gopath
 set CGO_ENABLED=0
 set GOARCH=amd64
 set GOOS=windows
-D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.8.windows-amd64\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_6_windows.exe
+D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.8.windows-amd64\bin\go.exe build -ldflags="-s -w -X github.com/minio/minio/cmd.Version=2025-10-15T17:29:55Z -X github.com/minio/minio/cmd.CopyrightYear=2025 -X github.com/minio/minio/cmd.ReleaseTag=RELEASE.2025-10-15T17-29-55Z -X github.com/minio/minio/cmd.CommitID=9e49d5e7a648f00e26f2246f4dc28e6b07f8c84a -X github.com/minio/minio/cmd.ShortCommitID=9e49d5e7a648 -X github.com/minio/minio/cmd.GOPATH=D:\dev\dev\sdk\gopath -X github.com/minio/minio/cmd.GOROOT=D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.8.windows-amd64" -o D:\dev\dev\project\minio\build\minio1_6_windows.exe
 set GOARCH=amd64
 set GOOS=linux
-D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.8.windows-amd64\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_6_linux
+D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.8.windows-amd64\bin\go.exe build -ldflags="-s -w -X github.com/minio/minio/cmd.Version=2025-10-15T17:29:55Z -X github.com/minio/minio/cmd.CopyrightYear=2025 -X github.com/minio/minio/cmd.ReleaseTag=RELEASE.2025-10-15T17-29-55Z -X github.com/minio/minio/cmd.CommitID=9e49d5e7a648f00e26f2246f4dc28e6b07f8c84a -X github.com/minio/minio/cmd.ShortCommitID=9e49d5e7a648 -X github.com/minio/minio/cmd.GOPATH=D:\dev\dev\sdk\gopath -X github.com/minio/minio/cmd.GOROOT=D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.8.windows-amd64" -o D:\dev\dev\project\minio\build\minio1_6_linux
 set GOARCH=amd64
 set GOOS=darwin
-D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.8.windows-amd64\bin\go.exe build -o D:\dev\dev\project\minio\build\minio1_6_darwin
+D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.8.windows-amd64\bin\go.exe build -ldflags="-s -w -X github.com/minio/minio/cmd.Version=2025-10-15T17:29:55Z -X github.com/minio/minio/cmd.CopyrightYear=2025 -X github.com/minio/minio/cmd.ReleaseTag=RELEASE.2025-10-15T17-29-55Z -X github.com/minio/minio/cmd.CommitID=9e49d5e7a648f00e26f2246f4dc28e6b07f8c84a -X github.com/minio/minio/cmd.ShortCommitID=9e49d5e7a648 -X github.com/minio/minio/cmd.GOPATH=D:\dev\dev\sdk\gopath -X github.com/minio/minio/cmd.GOROOT=D:\dev\dev\sdk\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.24.8.windows-amd64" -o D:\dev\dev\project\minio\build\minio1_6_darwin
